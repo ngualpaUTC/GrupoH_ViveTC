e2c11f2aa72d16f64471257a18d2308b5b458ace
aguanoquUTC <ana.guanoquiza07@gmail.com>
2016-10-29 19:13:45 -0500

    Se coloco elementos de menú en la pantalla inicial
    El título: Provincia de Cotopaxi
    El menú.- dentro del menú se encuentran los botones como:
    Datos Generales, División Política, Atractivos Turísticos y Mapa
    Turístico.

--- gen/com/grupoh_vivetc/R.java
+++ gen/com/grupoh_vivetc/R.java
@@ -982,7 +982,7 @@ containing a value of this type.
         public static final int quilotoa=0x7f020059;
     }
     public static final class id {
-        public static final int AbsoluteLayout1=0x7f050043;
+        public static final int AbsoluteLayout1=0x7f050048;
         public static final int action_bar=0x7f05001a;
         public static final int action_bar_activity_content=0x7f050015;
         public static final int action_bar_container=0x7f050019;
@@ -996,10 +996,14 @@ containing a value of this type.
         public static final int action_mode_bar=0x7f05002f;
         public static final int action_mode_bar_stub=0x7f05002e;
         public static final int action_mode_close_button=0x7f050022;
-        public static final int action_settings=0x7f050045;
+        public static final int action_settings=0x7f05004a;
         public static final int activity_chooser_view_content=0x7f050023;
         public static final int always=0x7f05000b;
         public static final int beginning=0x7f050011;
+        public static final int button1=0x7f050044;
+        public static final int button2=0x7f050045;
+        public static final int button3=0x7f050046;
+        public static final int button4=0x7f050047;
         public static final int checkbox=0x7f05002b;
         public static final int collapseActionView=0x7f05000d;
         public static final int default_activity_button=0x7f050026;
@@ -1023,7 +1027,7 @@ containing a value of this type.
         public static final int never=0x7f050009;
         public static final int none=0x7f050010;
         public static final int normal=0x7f050000;
-        public static final int progressBar1=0x7f050044;
+        public static final int progressBar1=0x7f050049;
         public static final int progress_circular=0x7f050034;
         public static final int progress_horizontal=0x7f050035;
         public static final int radio=0x7f05002d;
@@ -1046,6 +1050,7 @@ containing a value of this type.
         public static final int split_action_bar=0x7f05001c;
         public static final int submit_area=0x7f05003f;
         public static final int tabMode=0x7f050002;
+        public static final int textView1=0x7f050043;
         public static final int title=0x7f050029;
         public static final int title_container=0x7f050030;
         public static final int top_action_bar=0x7f05001e;
