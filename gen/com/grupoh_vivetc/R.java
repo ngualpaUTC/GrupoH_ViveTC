61e56bf07098c707c9c0b400ff21b768763dbfe4
ngualpaUTC <nellygualpa04@gmail.com>
2016-10-30 22:34:54 -0500

    Crear menu superior
    Botones,
    menu,
    explorar, y
    buscar

--- gen/com/grupoh_vivetc/R.java
+++ gen/com/grupoh_vivetc/R.java
@@ -983,13 +983,15 @@ containing a value of this type.
         public static final int ic_launcher=0x7f02005a;
         public static final int library=0x7f02005b;
         public static final int logo1=0x7f02005c;
-        public static final int mapa=0x7f02005d;
-        public static final int mundo=0x7f02005e;
-        public static final int panorama=0x7f02005f;
-        public static final int quilotoa=0x7f020060;
+        public static final int lupa=0x7f02005d;
+        public static final int mapa=0x7f02005e;
+        public static final int menu=0x7f02005f;
+        public static final int mundo=0x7f020060;
+        public static final int panorama=0x7f020061;
+        public static final int quilotoa=0x7f020062;
     }
     public static final class id {
-        public static final int RelativeLayout1=0x7f050049;
+        public static final int RelativeLayout1=0x7f05004c;
         public static final int action_bar=0x7f05001a;
         public static final int action_bar_activity_content=0x7f050015;
         public static final int action_bar_container=0x7f050019;
@@ -1003,14 +1005,17 @@ containing a value of this type.
         public static final int action_mode_bar=0x7f05002f;
         public static final int action_mode_bar_stub=0x7f05002e;
         public static final int action_mode_close_button=0x7f050022;
-        public static final int action_settings=0x7f05004b;
+        public static final int action_settings=0x7f05004e;
         public static final int activity_chooser_view_content=0x7f050023;
         public static final int always=0x7f05000b;
         public static final int beginning=0x7f050011;
-        public static final int button1=0x7f050045;
-        public static final int button2=0x7f050046;
-        public static final int button3=0x7f050047;
-        public static final int button4=0x7f050048;
+        public static final int button1=0x7f050048;
+        public static final int button2=0x7f050049;
+        public static final int button3=0x7f05004a;
+        public static final int button4=0x7f05004b;
+        public static final int button5=0x7f050045;
+        public static final int button6=0x7f050043;
+        public static final int button7=0x7f050044;
         public static final int checkbox=0x7f05002b;
         public static final int collapseActionView=0x7f05000d;
         public static final int default_activity_button=0x7f050026;
@@ -1026,7 +1031,7 @@ containing a value of this type.
         public static final int icon=0x7f050028;
         public static final int ifRoom=0x7f05000a;
         public static final int image=0x7f050025;
-        public static final int imageView1=0x7f050043;
+        public static final int imageView1=0x7f050046;
         public static final int left_icon=0x7f050031;
         public static final int listMode=0x7f050001;
         public static final int list_item=0x7f050027;
@@ -1034,7 +1039,7 @@ containing a value of this type.
         public static final int never=0x7f050009;
         public static final int none=0x7f050010;
         public static final int normal=0x7f050000;
-        public static final int progressBar1=0x7f05004a;
+        public static final int progressBar1=0x7f05004d;
         public static final int progress_circular=0x7f050034;
         public static final int progress_horizontal=0x7f050035;
         public static final int radio=0x7f05002d;
@@ -1058,7 +1063,7 @@ containing a value of this type.
         public static final int split_action_bar=0x7f05001c;
         public static final int submit_area=0x7f05003f;
         public static final int tabMode=0x7f050002;
-        public static final int textView1=0x7f050044;
+        public static final int textView1=0x7f050047;
         public static final int title=0x7f050029;
         public static final int title_container=0x7f050030;
         public static final int top_action_bar=0x7f05001e;
